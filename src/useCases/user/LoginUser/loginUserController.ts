0a1bba038109249ca2a2eb976d96acb8b2811970
miqueiasmartinsf <miqueias.martinsf@gmail.com>
2024-07-05 09:05:24 -0300

    add jwt as a dependency

--- src/useCases/user/LoginUser/loginUserController.ts
+++ src/useCases/user/LoginUser/loginUserController.ts
@@ -4,13 +4,15 @@ import { Request, Response } from "express";
 export class LoginUserController {
     constructor(private loginUserUseCase: LoginUserUseCase) {}
 
-    async handle(req: Request, res: Response): Promise<Response> {
+    handle = async (req: Request, res: Response): Promise<Response> => {
         const { email, password } = req.body;
 
         try {
             await this.loginUserUseCase.execute({ email, password });
-        } catch (error) {}
+        } catch (error) {
+            console.log(error);
+        }
 
         return res;
-    }
+    };
 }
--- src/useCases/user/LoginUser/loginUserUseCase.ts
+++ src/useCases/user/LoginUser/loginUserUseCase.ts
@@ -1,14 +1,27 @@
 import { IUserRepository } from "../../../interfaces/IUserRepository";
 import { LoginUserDTO } from "./loginUserDTO";
 import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
+import 'dotenv/config'
 
 export class LoginUserUseCase {
     constructor(private userRepository: IUserRepository) {}
 
-    execute = async (data: LoginUserDTO) => {
-        const userExists = this.userRepository.findByEmail(data.email);
+    execute = async (data: LoginUserDTO): Promise<string> => {
+        const userExists = await this.userRepository.findByEmail(data.email);
 
-        if (userExists !== null) {
+        if (userExists === null) {
+            throw new Error("User not signedup");
         }
+
+        if (
+            !bcrypt.compareSync(data.password, userExists.dataValues.password)
+        ) {
+            throw new Error("Incorrect password");
+        }
+
+        const token = jsonwebtoken.sign(data.email, process.env.JWT_SK);
+
+        return token;
     };
 }
